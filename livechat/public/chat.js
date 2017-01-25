74f5a0abfa43be29d593692e852d76b455c717fa
rockinrobin714 <robindykema@gmail.com>
2017-01-24 23:48:06 -0600

    Initialized firebase

--- livechat/public/chat.js
+++ livechat/public/chat.js
@@ -2,3 +2,10 @@ var myFirebase = new Firebase('https://chatlive-c8200.firebaseio.com/');
 var usernameInput = document.querySelector('#username');
 var textInput = document.querySelector('#text');
 var postButton = document.querySelector('#post');
+
+postButton.adEventListener('click', function(){
+	var msgUser = usernameInput.value;
+	var msgText = textInput.value;
+	myFirebase.set(msgUser + " says: " + msgText);
+	textInput.value = '';
+});
