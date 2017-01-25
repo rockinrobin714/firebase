0dc025be0d5da34562088df0a472610080eef927
rockinrobin714 <robindykema@gmail.com>
2017-01-24 23:31:44 -0600

    Set up firebase

---
+++ livechat/public/chat.js
@@ -0,0 +1,4 @@
+var myFirebase = new Firebase('https://chatlive-c8200.firebaseio.com/');
+var usernameInput = document.querySelector('#username');
+var textInput = document.querySelector('#text');
+var postButton = document.querySelector('#post');
